afb3f158067510acd6b0a43d58910b49bb0a297d
dudu <dudu@ecoearn.io>
2024-07-10 15:23:12 +0800

    feat: add privacy policy page

--- src/pageComponents/privacy-policy/index.tsx
+++ src/pageComponents/privacy-policy/index.tsx
@@ -222,6 +222,243 @@ export default function PrivacyPolicy() {
             We may process or share your information under the following circumstances or as
             otherwise outlined in this Privacy Policy:
           </p>
+          <ul>
+            <li>
+              Consent – if you have given us specific consent to use your personal information for a
+              specific purpose.
+            </li>
+            <li>
+              Legitimate Interests – when it is reasonably necessary to achieve our legitimate
+              business interests.
+            </li>
+            <li>
+              Performance of a Contract – Where we have entered into a contract with you, we may
+              process your personal information to fulfill the terms of our contract.
+            </li>
+            <li>
+              Legal Obligations – where we are legally required to do so in order to comply with
+              applicable law, governmental requests, a judicial proceeding, court order, or legal
+              process, such as in response to a court order or a subpoena (including in response to
+              public authorities to meet national security or law enforcement requirements).
+            </li>
+            <li>
+              Vital Interests – where we believe it is necessary to investigate, prevent, or take
+              action regarding potential violations of our policies, suspected fraud, situations
+              involving potential threats to the safety of any person and illegal activities, or as
+              evidence in litigation in which we are involved.
+            </li>
+          </ul>
+          <p>
+            More specifically, we may need to process your information or share your personal
+            information in the following situations:
+          </p>
+          <ul>
+            <li>
+              Business Transfers – We may share or transfer your information in connection with, or
+              during negotiations of, any merger, sale of company assets, financing, or acquisition
+              of all or a portion of our business to another company.
+            </li>
+            <li>
+              Vendors, Consultants, and Other Third-Party Service Providers – We may share your data
+              with third-party vendors, service providers, contractors or agents who perform
+              services for us or on our behalf and require access to such information to do that
+              work. Examples include: payment processing, data analysis, email delivery, hosting
+              services, customer service and marketing efforts. We may allow selected third parties
+              to use tracking technology on the Website, which will enable them to collect data on
+              our behalf about how you interact with our Website over time. This information may be
+              used to, among other things, analyse and track data, determine the popularity of
+              certain content, pages or features, and better understand online activity. Unless
+              described in this privacy policy , we do not share, sell, rent or trade any of your
+              information with third parties for their promotional purposes.
+            </li>
+            <li>
+              Affiliates – We may share your information with our affiliates, in which case we will
+              require those affiliates to honour this privacy policy. Affiliates include our parent
+              company and any subsidiaries, joint venture partners or other companies that we
+              control or that are under common control with us.
+            </li>
+            <li>
+              Business Partners – We may share your information with our business partners to offer
+              you certain products, services, or promotions.
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h2>6. Who Will Your Information Be Shared With?</h2>
+          <p>
+            We only share and disclose your information with the following categories of third
+            parties. If we have processed your data based on your consent and you wish to revoke
+            your consent, please contact us using the contact details provided in section 14 below.
+          </p>
+          <ul>
+            <li>Data Analytics Service Providers</li>
+            <li>Data Storage Service Providers</li>
+            <li>Cloud Computing Service Providers</li>
+            <li>Communication & Collaboration Tools</li>
+            <li>Performance Monitoring Tools</li>
+            <li>Product Engineering & Design Tools</li>
+            <li>Social Networks</li>
+            <li>Sales & Marketing Tools</li>
+            <li>Testing Tools</li>
+            <li>Website Hosting Service Providers</li>
+          </ul>
+        </section>
+        <section>
+          <h2>7. Use Cookies And Other Tracking Technologies?</h2>
+          <p>
+            We do not use cookies or tracking technologies. Rather, we utilise anonymous page view
+            tracking information provided by third-party analytics providers which do not collect or
+            store any personal data. As such, we make no use of cookies.
+          </p>
+        </section>
+        <section>
+          <h2>8. Transfer of Information to Other Jurisdictions</h2>
+          <p className="mt-2">
+            EcoEarn operates and engages service providers in various jurisdictions. Therefore, we
+            and our service providers may transfer your personal information to, or store or access
+            it in, jurisdictions that may not provide levels of data protection that are equivalent
+            to those of your home jurisdiction. By using our Website, you acknowledge and agree to
+            such transfers and processing, including to and in the United States. We will take steps
+            to ensure that your personal information receives an adequate level of protection in the
+            jurisdictions in which we process it.
+          </p>
+          <p className="mt-2">
+            If you are a resident in the European Economic Area (EEA) or United Kingdom (UK), then
+            these countries may not necessarily have data protection laws or other similar laws as
+            comprehensive as those in your country. We will, however, take all necessary measures to
+            protect your personal information in accordance with this privacy policy and applicable
+            laws.
+          </p>
+          <ul>
+            <li>
+              European Commission’s Standard Contractual ClauseWe have implemented measures to
+              protect your personal information, including by using the European Commission’s
+              Standard Contractual Clauses for transfers of personal information between our group
+              companies and between us and our third-party providers. These clauses require all
+              recipients to protect all personal information that they process originating from the
+              EEA or UK in accordance with European data protection laws and regulations. Our
+              Standard Contractual Clauses can be provided upon request. We have implemented similar
+              appropriate safeguards with our third-party service providers and partners and further
+              details can be provided upon request.
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h2>9. Duration of Storage of Information</h2>
+          <p className="mt-2">
+            We will only keep your personal information for as long as it is necessary for the
+            purposes set out in this Privacy Policy, unless a longer retention period is required or
+            permitted by law (such as tax, accounting or other legal requirements). No purpose in
+            this Privacy Policy will require us keeping your personal information for longer than 2
+            years.
+          </p>
+          <p className="mt-2">
+            When we have no ongoing legitimate business need to process your personal information,
+            we will either delete or anonymize such information, or, if this is not possible (for
+            example, because your personal information has been stored in backup archives), then we
+            will securely store your personal information and isolate it from any further processing
+            until deletion is possible.
+          </p>
+        </section>
+        <section>
+          <h2>10. Maintaining Security of the Information</h2>
+          <p className="mt-2">
+            We have implemented appropriate technical and organizational security measures designed
+            to protect the security of any personal information we collect and process.
+          </p>
+          <p>However, please remember:</p>
+          <ul>
+            <li>
+              Despite our safeguards and efforts to secure your information, unfortunately no
+              electronic transmission over the Internet or information storage technology can be
+              guaranteed to be 100% secure, so we cannot promise or guarantee that hackers,
+              cybercriminals, or other unauthorized third parties will not be able to defeat our
+              security, and improperly collect, access, steal, or modify your information.
+            </li>
+            <li>
+              Transmission of personal information to and from our Website is at your own risk. You
+              should only access the Website within a secure environment.
+            </li>
+            <li>You are responsible for your username and password and should keep them safe.</li>
+            <li>
+              If you believe your privacy has been breached, please contact us immediately{' '}
+              <a href="https://form.aelf.com/contact" target="_blank" rel="noreferrer">
+                here
+              </a>
+              .
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h2>11. Your Rights and Choices</h2>
+          <p>
+            In some regions (like the EEA and UK), you have certain rights under applicable data
+            protection laws. These may include the right to :
+          </p>
+          <ul>
+            <li>Request access and obtain a copy of your personal information.</li>
+            <li>Request rectification or erasure.</li>
+            <li>
+              Restrict the processing of your personal information and, if applicable, to data
+              portability.
+            </li>
+            <li>
+              In certain circumstances, you may also have the right to object to the processing of
+              your personal information.
+            </li>
+            <li>
+              If we are relying on your consent to process your personal information, you have the
+              right to withdraw your consent at any time. Please note however that this will not
+              affect the lawfulness of the processing before its withdrawal, nor will it affect the
+              processing of your personal information conducted in reliance on lawful processing
+              grounds other than consent.
+            </li>
+          </ul>
+        </section>
+        <section>
+          <h2>12. Controls For Do-not-track Features</h2>
+          <p>
+            Most web browsers and some mobile operating systems and mobile applications include a
+            Do-Not-Track (“DNT”) feature or setting you can activate to signal your privacy
+            preference not to have data about your online browsing activities monitored and
+            collected. At this stage no uniform technology standard for recognizing and implementing
+            DNT signals has been finalized. As such, we do not currently respond to DNT browser
+            signals or any other mechanism that automatically communicates your choice not to be
+            tracked online. If a standard for online tracking is adopted that we must follow in the
+            future, we will inform you about that practice in a revised version of this privacy
+            policy.
+          </p>
+        </section>
+        <section>
+          <h2>13. Changes To This Privacy Policy</h2>
+          <p>
+            We may update this Privacy Policy from time to time. The updated version will be
+            indicated by an updated “Revised” date and the updated version will be effective as soon
+            as it is accessible. If we make material changes to this privacy policy, we may notify
+            you either by prominently posting a notice of such changes or by directly sending you a
+            notification. We encourage you to review this Privacy Policy frequently to be informed
+            of how we are protecting your information.
+          </p>
+        </section>
+        <section>
+          <h2>14. Contact Us</h2>
+          <p>
+            If you have questions or comments about this privacy policy, you may email us{' '}
+            <a href="mailto:contact@ecoearn.io" target="_blank" rel="noreferrer">
+              here
+            </a>
+            .
+          </p>
+        </section>
+        <section>
+          <h2>15. How Can You Review, Update, Or Delete The Data We Collect From You?</h2>
+          <p>
+            Based on the applicable laws of your country, you may have the right to request access
+            to the personal information we collect from you, change that information, have any
+            inaccurate information corrected or delete it in some circumstances. To request to
+            review, update, or delete your personal information, please contact us on the above
+            provided details.
+          </p>
         </section>
       </article>
     </div>
