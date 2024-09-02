03cc5f5ce1a6297741ea1ea91bce078ec29f08da
dudu <dudu@ecoearn.io>
2024-09-02 10:01:23 +0800

    feat: remove close modal

--- src/components/StakeModal/index.tsx
+++ src/components/StakeModal/index.tsx
@@ -672,7 +672,6 @@ function StakeModal({
       selectTokenInSymbol: 'ELF',
       selectTokenOutSymbol: stakeSymbol,
       onCancel: async () => {
-        swapModal.hide();
         const curBalance = await fetchBalance?.();
         curBalance && setBalance(curBalance);
       },
