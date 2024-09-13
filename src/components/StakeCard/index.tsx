73c41f7b054875e612fd8df101b8693a7426b8e8
donny-huo <dustin@eforest.finance>
2024-09-13 21:13:23 +0800

    feat: stakeCard

--- src/components/StakeCard/index.tsx
+++ src/components/StakeCard/index.tsx
@@ -35,42 +35,24 @@ interface IStakeCardProps {
   onRenewal?: (data: IStakePoolData) => void;
 }
 
-export default function StakeCard({
-  type,
-  data,
-  renewText,
-  isLogin,
-  onStake,
-  onAdd,
-  onClaim,
-  onExtend,
-  onUnlock,
-  onRenewal,
-}: IStakeCardProps) {
+export default function StakeCard({ type, data, isLogin }: IStakeCardProps) {
   const {
     projectOwner,
     aprMax,
     aprMin,
     earnedSymbol,
-    totalStake,
     totalStakeInUsd,
     stakeSymbol,
-    earned = '0',
-    earnedInUsd,
     staked,
-    stakedInUsd,
-    stakeApr,
     icons,
     decimal = 8,
     rate,
     unlockWindowDuration,
     stakingPeriod,
     lastOperationTime,
-    minimumClaimAmount,
-    latestClaimTime,
   } = data;
 
-  const { countDisplay, isUnLocked, targetUnlockTimeStamp } = useUnlockCount({
+  const { isUnLocked } = useUnlockCount({
     unlockWindowDuration: unlockWindowDuration || 0,
     stakingPeriod: stakingPeriod || 0,
     lastOperationTime: lastOperationTime || 0,
@@ -91,56 +73,12 @@ export default function StakeCard({
     return `Up to ${formatNumberWithDecimalPlaces(ZERO.plus(aprMax).times(100))}%`;
   }, [aprMax, aprMin]);
 
-  const isClaimed = useMemo(() => {
-    return dayjs(latestClaimTime || 0).isAfter(targetUnlockTimeStamp);
-  }, [latestClaimTime, targetUnlockTimeStamp]);
-
-  const canClaim = useMemo(() => {
-    if (isUnLocked && !isClaimed) {
-      return ZERO.plus(divDecimals(earned, decimal)).gt(0);
-    }
-    return false;
-  }, [decimal, earned, isClaimed, isUnLocked]);
-
-  const claimBtnTip = useMemo(() => {
-    if (isUnLocked) {
-      return isClaimed ? 'Rewards will be claimed simultaneously upon unlocking.' : '';
-    }
-    return 'You cannot claim rewards during the lock-up period for staking.';
-  }, [isClaimed, isUnLocked]);
-
-  const unStakeTip = useMemo(
-    () => (!isUnLocked ? 'You cannot unlock during the lock-up period for staking.' : ''),
-    [isUnLocked],
-  );
-
-  const stakingExpiredTip = useMemo(
-    () =>
-      isUnLocked ? 'Stake has expired, cannot be added stake. Please renew the staking first.' : '',
-    [isUnLocked],
-  );
-
-  const stakedStr = useMemo(() => {
-    const stakeAmount = divDecimals(staked, decimal);
-    return formatNumber(stakeAmount);
-  }, [decimal, staked]);
-
-  const disabledExtendBtn = useMemo(() => {
-    const remainingDays = dayjs(targetUnlockTimeStamp).diff(dayjs(), 'hour') / 24;
-    const canNotExtends = ZERO.plus(remainingDays || 0).gt(MAX_STAKE_PERIOD - 1);
-    return isUnLocked || canNotExtends;
-  }, [isUnLocked, targetUnlockTimeStamp]);
-
-  const earnAmountText = useMemo(() => {
-    return formatNumber(divDecimals(earned, decimal));
-  }, [decimal, earned]);
-
   const displayEarnSymbol = useMemo(() => {
     return formatTokenSymbol(earnedSymbol || '');
   }, [earnedSymbol]);
 
   return (
-    <div className="stake-card lg:w-[443px] px-4 py-4 md:px-8 md:py-8 rounded-xl border border-solid border-neutralDivider bg-neutralWhiteBg transition-all ease-in-out duration-300 hover:shadow-xl hover:ease hover:duration-300 group">
+    <div className="stake-card lg:min-w-[443px] md:min-w-[443px] px-4 py-4 md:px-8 md:py-8 rounded-xl border border-solid border-neutralDivider bg-neutralWhiteBg transition-all ease-in-out duration-300 hover:shadow-xl hover:ease hover:duration-300 group">
       <div className="flex !items-center justify-between lg:flex-row lg:items-start">
         <StakeToken
           type={type}
@@ -203,141 +141,6 @@ export default function StakeCard({
           </Button>
         </div>
       </div>
-      {/* {showStakeInfo && isUnLocked !== null && (
-        <div className="relative flex flex-col px-4 pt-10 pb-6 gap-6 md:flex-row md:justify-between bg-brandFooterBg md:px-8 md:py-8 lg:gap-8 rounded-xl">
-          <ToolTip title="APR from Staking">
-            <div className={styles['apr-tag']}>
-              <span className={styles['apr-text']}>
-                APR: {formatNumberWithDecimalPlaces(BigNumber(stakeApr || '').times(100)) || '--'}%
-              </span>
-            </div>
-          </ToolTip>
-
-          <div className="flex flex-1 flex-col xl:flex-row justify-between md:max-w-[336px] gap-6 md:gap-4">
-            <div className="flex justify-between md:flex-col md:justify-start md:gap-2">
-              <div className="text-base text-neutralSecondary font-medium">
-                <span>Earned</span>
-              </div>
-              <div className="flex flex-col gap-1 items-end md:items-start">
-                <div className="text-base font-semibold text-neutralTitle flex items-center gap-1">
-                  <span>{earnAmountText}</span>
-                  <span>
-                    <TextEllipsis text={displayEarnSymbol || ''} digits={10} />
-                  </span>
-                </div>
-                <div className="text-sm font-medium text-neutralTitle">
-                  {formatUSDPrice(divDecimals(earnedInUsd || 0, decimal))}
-                </div>
-              </div>
-            </div>
-            <ToolTip title={claimBtnTip} overlayStyle={{ maxWidth: 205 }}>
-              <Button
-                className="lg:w-[100px] !rounded-md xl:!mt-2"
-                type="primary"
-                ghost={canClaim}
-                size="medium"
-                disabled={!canClaim}
-                onClick={() => {
-                  onClaim?.(data);
-                }}
-              >
-                Claim
-              </Button>
-            </ToolTip>
-          </div>
-          <div className="h-[1px] w-full md:w-[1px] md:h-[inherit] xl:h-[100px] bg-neutralDivider"></div>
-          <div className="flex flex-1 flex-col xl:flex-row justify-between md:max-w-[336px] gap-6 md:gap-4">
-            <div className="flex justify-between md:flex-col md:justify-start md:gap-2">
-              <div className="text-base text-neutralSecondary font-medium">Staked</div>
-              <div className="flex flex-col gap-1 items-end md:items-start">
-                <div className="flex gap-1 text-base font-semibold text-neutralTitle flex-grow-0">
-                  <ToolTip title="The number of rewards included">
-                    <span>{stakedStr}</span>
-                  </ToolTip>
-                  <span>
-                    <TextEllipsis text={formatTokenSymbol(stakeSymbol || '')} digits={10} />
-                  </span>
-                </div>
-                <div className="text-sm font-medium text-neutralTitle">
-                  {formatUSDPrice(divDecimals(stakedInUsd || 0, decimal))}
-                </div>
-              </div>
-            </div>
-            <div className="flex gap-4 md:gap-3 xl:flex-col xl:mt-2">
-              <ToolTip title={stakingExpiredTip}>
-                <Button
-                  className="flex-1 lg:flex-initial lg:w-[100px] !rounded-md"
-                  type="primary"
-                  size="medium"
-                  onClick={() => {
-                    onAdd?.(data);
-                  }}
-                  disabled={!!isUnLocked}
-                >
-                  Add
-                </Button>
-              </ToolTip>
-              <ToolTip title={unStakeTip}>
-                <Button
-                  className="flex-1 lg:flex-initial lg:w-[100px] !rounded-md"
-                  size="medium"
-                  disabled={!isUnLocked}
-                  onClick={() => {
-                    onUnlock?.(data);
-                  }}
-                >
-                  Unlock
-                </Button>
-              </ToolTip>
-            </div>
-          </div>
-          <div className="h-[1px] w-full md:w-[1px] md:h-[inherit] xl:h-[100px] bg-neutralDivider"></div>
-          <div className="flex flex-1 flex-col gap-6 xl:flex-row md:gap-4 md:max-w-[336px] justify-between">
-            {isUnLocked ? (
-              <Renewal
-                unlockTimeStamp={targetUnlockTimeStamp || ''}
-                unlockWindowDuration={unlockWindowDuration || ''}
-                renewText={renewText}
-                onRenewal={() => {
-                  onRenewal?.(data);
-                }}
-              />
-            ) : (
-              <>
-                <div className="flex flex-col gap-2 lg:min-w-[210px]">
-                  <div className="text-base text-neutralSecondary font-medium">
-                    Remaining Lock-up Period
-                  </div>
-                  <div className="flex flex-col gap-1">
-                    <div className="text-base font-semibold text-neutralTitle">
-                      {isUnLocked ? 'Unlockable' : countDisplay}
-                    </div>
-                    {!!targetUnlockTimeStamp && (
-                      <div className="text-sm font-medium text-neutralDisable">
-                        Unlock on {dayjs(targetUnlockTimeStamp).format(DEFAULT_DATE_FORMAT)}
-                      </div>
-                    )}
-                  </div>
-                </div>
-                <ToolTip title={stakingExpiredTip}>
-                  <Button
-                    className="!rounded-md xl:w-[100px] xl:!mt-2"
-                    type="primary"
-                    size="medium"
-                    ghost
-                    disabled={disabledExtendBtn}
-                    onClick={() => {
-                      onExtend?.(data);
-                    }}
-                  >
-                    Extend
-                  </Button>
-                </ToolTip>
-              </>
-            )}
-          </div>
-        </div>
-      )} */}
     </div>
   );
 }
